f551d7b78dd4e58dd279f81175b6d6034f9f1b3e
emmillykaythe <kaytheemmilly@gmail.com>
2025-06-29 16:46:00 -0300

    responsividade na aba lateral

--- src/app/page.js
+++ src/app/page.js
@@ -8,19 +8,6 @@ export default function Home() {
         <div className = {styles.abaLateral} >
         <Image className ={styles.logo} src="/logo-principal.png" width="350" height="350" alt="Logo" />
     </div>      
-        <div className={styles.container}>
-        <Image className = {styles.usuario} src="/usuario.png" width="370" height="200" alt="usuario" /> 
-
-    <div className={styles.caixaform}>
-      <input className={styles.input} type="email" placeholder="Digite seu email" />
-      <input className={styles.input} type="password" placeholder="Digite sua senha" />
-    </div>
-      <button className={styles.button}>LOGIN</button>
-      
-      <Link href ='/cadastro'> 
-      <button className={styles.button}>SIGN UP</button>
-      </Link>
-    </div>
     </div>
   );
 }
