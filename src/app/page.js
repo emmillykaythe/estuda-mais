8eb5066193801fd777bce580b3fb043dc0289b83
emmilly <kaytheemmilly@gmail.com>
2025-08-19 08:38:49 -0300

    adição de visualização de senha na tela de login

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
   <div className={styles.page}>
     <div className = {styles.abaLateral} >
@@ -12,7 +17,14 @@ export default function Home() {
       <Image className = {styles.usuario} src="/usuario-login.png" width="370" height="200" alt="usuario" />
     <div className={styles.caixaform}>
       <input className={styles.input} type="email" placeholder="Digite seu email" />
-      <input className={styles.input} type="password" placeholder="Digite sua senha" />
+    
+    <div className={styles.senha}>  
+      <input className={styles.input} type={showPassword ? "text" : "password"} placeholder="Digite sua senha" />
+        <span className={styles.visualizarSenha}
+          onClick={() => setShowPassword(!showPassword)}>
+        <Image src = {showPassword ? "/PermitirVisualizacao-cadastro.png" : "/BloquearVisualizacao-cadastro.png"} width={20} height={20} alt="Visualizar Senha"/>
+        </span>
+      </div>
   </div>
         
         <Link href ='/inicio' className={styles.button}> ENTRAR </Link>
