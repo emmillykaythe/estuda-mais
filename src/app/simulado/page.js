05a48b4403ef0816ec98a52e83b6adeee9d08c79
emmillykaythe <kaytheemmilly@gmail.com>
2025-10-19 21:50:17 -0300

    exclusão e organização dos simulados

--- src/app/simulado/page.js
+++ src/app/simulado/page.js
@@ -3,20 +3,8 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 const dados = [
-  { title: "Simulado - Enem 2011", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2012", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2013", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2014", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2015", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2016", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2017", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2018", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2019", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2020", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2021", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2022", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2023", description: "45 questões de matemática e suas tecnologias" },
-  { title: "Simulado - Enem 2024", description: "45 questões de matemática e suas tecnologias" },
+  { title: "Simulado - IFPB 2024", description: "45 questões de matemática e suas tecnologias" },
+  { title: "Simulado - IFPB 2025", description: "45 questões de matemática e suas tecnologias" },
 ];
 
 function Assuntos({ title, description }) {
@@ -29,7 +17,7 @@ function Assuntos({ title, description }) {
         </div>
       </div>
       <p>{description}</p>
-      <button className={styles.botaoIniciar}>Iniciar</button>
+     <Link href="/questoes" className={styles.buttonSemEstilo}> <button className={styles.botaoIniciar}>Iniciar</button> </Link>
     </div>
   );
 }
@@ -37,12 +25,13 @@ function Assuntos({ title, description }) {
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Link href="/material"> 
         <div className={styles.voltar}>
+      <Link href="/inicio"> 
           <Image className={styles.buttonVoltar} src="/voltar.png" width="20" height="20" alt="Voltar" />
+    </Link>
        </div>
-      </Link>
-          <Image className={styles.logo} src="/logo-original.svg" width="150" height="150" alt="Logo" />
+    
+          <Image className={styles.logo} src="/logo-original.svg" width="170" height="170" alt="Logo" />
       <div className={styles.container}>
         {dados.map((assuntos, index) => (
           <Assuntos key={index} title={assuntos.title} description={assuntos.description} />
