7c0e1ec920eab8da8ea64db38a70c24da50975e2
emmillykaythe <kaytheemmilly@gmail.com>
2025-11-27 23:02:42 -0300

    removendo rotas inválidas

--- middleware.js
+++ middleware.js
@@ -11,34 +11,23 @@ export async function middleware(req) {
         pathname.startsWith("/questoes") ||
         pathname.startsWith("/simulado");
 
-    //caso seja necessário autenticação e não se tenha um token
-    //então se monta uma URL de redirecionamento para a página de login e depopis
-    //adiciona callbackUrl para, após logar, você poder voltar o 
-    // usuário para onde ele estava indo.
     if (precisaAuth && !token) {
         const url = new URL("/", req.url);
         url.searchParams.set("callbackUrl", pathname);
         return NextResponse.redirect(url);
     }
 
-    //para as suas rotas de admin
-    //primeiramente se não estiver logado (!token), manda para fazer login.
-    //Depois quando estiver logado mas não for admin (token.role !== "admin"), então redireciona 
-    //para uma página de acesso negado (/nao-autorizado).
     if (pathname.startsWith("/admin")) {
         if (!token) return NextResponse.redirect(new URL("/", req.url));
         if (token.role !== "admin")
-            return NextResponse.redirect(new URL("/nao-autorizado", req.url)); //não_autorizado seria oq?
+            return NextResponse.redirect(new URL("/nao-autorizado", req.url)); 
     }
 
     return NextResponse.next();
 }
 
-//O middleware vai interceptar essas rotas, sendo que o
-// :path* significa: qualquer subrota.
 export const config = {
     matcher: [
-        "//:path*",
         "/inicio/:path*",
         "/material/:path*",
         "/questoes/:path*",
